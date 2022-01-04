da92ebbd1803dfdfaf15e0fcec4628d9663f1f04
Thomas Denewiler <tdenewiler@gmail.com>
2022-01-04 09:03:05 -0800

    Update version and CHANGELOG for new release: v0.1.3. (#22)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author="Thomas Denewiler",
     name="statick-planning",
     description="Statick analysis plugins for planning files.",
-    version="0.1.2",
+    version="0.1.3",
     packages=[
         "statick_tool",
         "statick_tool.plugins.discovery",
