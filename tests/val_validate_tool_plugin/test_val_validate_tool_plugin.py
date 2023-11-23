4bffb447828d6e62d22b37cd1f51531f6b8e5a62
Thomas Denewiler <tdenewiler@gmail.com>
2023-11-23 15:30:51 -0800

    Switch from setup.py to pyproject.toml, update unit tests, update github workflows.

--- tests/val_validate_tool_plugin/test_val_validate_tool_plugin.py
+++ tests/val_validate_tool_plugin/test_val_validate_tool_plugin.py
@@ -1,20 +1,22 @@
 """Unit tests for the Validate tool plugin."""
 import argparse
+import mock
 import os
+import pytest
 import subprocess
 import sys
-
-import mock
-import pytest
-from yapsy.PluginManager import PluginManager
 
 import statick_tool
 from statick_tool.config import Config
 from statick_tool.package import Package
 from statick_tool.plugin_context import PluginContext
-from statick_tool.plugins.tool.val_validate_tool_plugin import ValValidateToolPlugin
+from statick_tool.plugins.tool.val_validate import ValValidateToolPlugin
 from statick_tool.resources import Resources
-from statick_tool.tool_plugin import ToolPlugin
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
 
 
 def setup_val_validate_tool_plugin(binary=None):
@@ -44,27 +46,13 @@ def setup_val_validate_tool_plugin(binary=None):
 
 def test_val_validate_tool_plugin_found():
     """Test that the plugin manager finds the Validate plugin."""
-    manager = PluginManager()
-    # Get the path to statick_tool/__init__.py, get the directory part, and
-    # add 'plugins' to that to get the standard plugins dir
-    manager.setPluginPlaces(
-        [os.path.join(os.path.dirname(statick_tool.__file__), "plugins")]
-    )
-    manager.setCategoriesFilter(
-        {
-            "Tool": ToolPlugin,
-        }
-    )
-    manager.collectPlugins()
-    # Verify that a plugin's get_name() function returns "val_validate"
+    tool_plugins = {}
+    plugins = entry_points(group="statick_tool.plugins.tool")
+    for plugin_type in plugins:
+        plugin = plugin_type.load()
+        tool_plugins[plugin_type.name] = plugin()
     assert any(
-        plugin_info.plugin_object.get_name() == "val_validate"
-        for plugin_info in manager.getPluginsOfCategory("Tool")
-    )
-    # While we're at it, verify that a plugin is named Validate Tool Plugin
-    assert any(
-        plugin_info.name == "VAL Validate Tool Plugin"
-        for plugin_info in manager.getPluginsOfCategory("Tool")
+        plugin.get_name() == "val_validate" for _, plugin in list(tool_plugins.items())
     )
 
 
@@ -198,7 +186,7 @@ def test_val_validate_tool_plugin_parse_invalid():
 
 
 @mock.patch(
-    "statick_tool.plugins.tool.val_validate_tool_plugin.subprocess.check_output"
+    "statick_tool.plugins.tool.val_validate.subprocess.check_output"
 )
 def test_val_validate_tool_plugin_scan_calledprocesserror(mock_subprocess_check_output):
     """
@@ -233,7 +221,7 @@ def test_val_validate_tool_plugin_scan_calledprocesserror(mock_subprocess_check_
 
 
 @mock.patch(
-    "statick_tool.plugins.tool.val_validate_tool_plugin.subprocess.check_output"
+    "statick_tool.plugins.tool.val_validate.subprocess.check_output"
 )
 def test_val_validate_tool_plugin_scan_oserror(mock_subprocess_check_output):
     """
